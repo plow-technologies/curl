ece780d3b62cf3f2dd0ec8fd7c1b8a463e0f8b7d
Rory Tyler Hayford <rory.hayford@protonmail.com>
2023-04-14 15:03:33 +0700

    Use `MonadIO` for public API

--- src/Curl.hs
+++ src/Curl.hs
@@ -13,50 +13,53 @@ module Curl
 where
 
 import Control.Monad (void)
+import Control.Monad.IO.Class (MonadIO (liftIO))
 import qualified Curl.Internal as Internal
 import Curl.Internal.Easy
 import Curl.Internal.Opts
 import Curl.Internal.Types
 import Curl.Opts as M
-import Curl.Types as M hiding (CookieList, Filetime, Private) -- conflicts
+import Curl.Types as M hiding (CookieList, Filetime, Private)
 import Data.ByteString.Lazy (ByteString)
 import Prelude hiding (head)
 
 -- | Run a @GET@ request and collect the body as a lazy 'ByteString'
-get :: UrlString -> [CurlOption] -> IO (CurlCode, ByteString)
-get url = runCurl . Internal.curlGetString url
+get :: MonadIO m => UrlString -> [CurlOption] -> m (CurlCode, ByteString)
+get url = liftIO . runCurl . Internal.curlGetString url
 
 -- | Run a @GET@ request, dumping the output on stdout
-get_ :: UrlString -> [CurlOption] -> IO CurlCode
-get_ url = runCurl . Internal.curlGet url
+get_ :: MonadIO m => UrlString -> [CurlOption] -> m CurlCode
+get_ url = liftIO . runCurl . Internal.curlGet url
 
 -- | Run a @HEAD@ request, gathering response info into a 'CurlResponse'
-head :: UrlString -> [CurlOption] -> IO CurlResponse
-head url = runCurl . Internal.curlHead url
+head :: MonadIO m => UrlString -> [CurlOption] -> m CurlResponse
+head url = liftIO . runCurl . Internal.curlHead url
 
 -- | Run a @POST@ request, returning the 'CurlCode'
-post :: UrlString -> [String] -> IO CurlCode
-post url = runCurl . Internal.curlPost url
+post :: MonadIO m => UrlString -> [String] -> m CurlCode
+post url = liftIO . runCurl . Internal.curlPost url
 
 -- | Run a @POST@ request, discarding the final 'CurlCode'
-post_ :: UrlString -> [String] -> IO ()
-post_ url = void . runCurl . Internal.curlPost url
+post_ :: MonadIO m => UrlString -> [String] -> m ()
+post_ url = liftIO . void . runCurl . Internal.curlPost url
 
 -- | Run a multipart @POST@ request, returning the 'CurlCode'
-multipart :: UrlString -> [CurlOption] -> [HttpPost] -> IO CurlCode
-multipart url opts = runCurl . Internal.curlMultipart url opts
+multipart :: MonadIO m => UrlString -> [CurlOption] -> [HttpPost] -> m CurlCode
+multipart url opts = liftIO . runCurl . Internal.curlMultipart url opts
 
 -- | Run a multipart @POST@ request, discarding the final 'CurlCode'
-multipart_ :: UrlString -> [CurlOption] -> [HttpPost] -> IO ()
-multipart_ url opts = void . runCurl . Internal.curlMultipart url opts
+multipart_ :: MonadIO m => UrlString -> [CurlOption] -> [HttpPost] -> m ()
+multipart_ url opts = liftIO . void . runCurl . Internal.curlMultipart url opts
 
 -- | Run a custom curl request (as specified by the 'CurlOption's), returning
 -- the 'CurlReponse'
-runWithResponse :: UrlString -> [CurlOption] -> IO CurlResponse
-runWithResponse url = runCurl . Internal.runWithResponse url
+runWithResponse :: MonadIO m => UrlString -> [CurlOption] -> m CurlResponse
+runWithResponse url = liftIO . runCurl . Internal.runWithResponse url
 
 -- | Run a custom curl request (as specified by the 'CurlOption's), returning
 -- the 'CurlReponse'. Also includes the 'InfoValue's taken from the provided
 -- 'Info's
-runWithResponseInfo :: UrlString -> [CurlOption] -> [Info] -> IO CurlResponse
-runWithResponseInfo url opts = runCurl . Internal.runWithResponseInfo url opts
+runWithResponseInfo ::
+  MonadIO m => UrlString -> [CurlOption] -> [Info] -> m CurlResponse
+runWithResponseInfo url opts =
+  liftIO . runCurl . Internal.runWithResponseInfo url opts
